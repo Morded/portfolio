73ed44a5a565c9b26f4cad953929abe6b44186fd
Gabor Jenei <gab.jenei@gmail.com>
2022-11-28 17:53:23 +0100

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,151 @@
+export const info = {
+  baseUrl: "https://gaborjenei.vercel.app",
+  name: "GÁBOR JENEI",
+  jobDescription: "App Developer",
+  about: `I took interest in some sort of programming
+    as a child. Started making my own websites
+    for what I felt passionate about, from Gaming
+    community websites to movie catalog
+    websites.
+    In my early 20s I took a more serious interest
+    in programming and wanted to do it as a
+    profession. I learned everything on my own
+    and got a job in the industry as a Junior
+    developer. Over the years I worked on all
+    kinds of projects and tried to acquired as
+    many skills as possible.
+
+    I love learning new things and I’m always
+    interested in new technologies.
+    I imagine myself in the future as either a front
+    end or a full stack developer  
+  `,
+
+  experience: [
+    {
+      name: "ION SO4EP kft.",
+      location: "Budapest, Hungary",
+      startDate: "2017",
+      endDate: "2022",
+      description: [
+        "Building features and modules into WMS, ERP and Renting Systems on both Android and PC.",
+        "Worked on the new company website. And all kinds of uniquely developed systems.",
+      ],
+      link: "https://ionerp.hu",
+      linkText: "See website"
+    },
+  ],
+
+  education: [
+    {
+      name: "Hardvard university",
+      location: "CS50x",
+      startDate: "2022",
+      endDate: "2022",
+      description: [
+        "Introduction to the intellectual enterprises of computer science and the art of programming.",
+        "This course teaches students how to think algorithmically and solve problems efficiently.",
+        "Topics include abstraction, algorithms, data structures, encapsulation, resource management, security, software engineering, and web programming.",
+        "Languages include C, Python, and SQL plus HTML, CSS, and JavaScript. ",
+        "Course culminates in a final project.",
+      ],
+      link: "https://cs50.harvard.edu/certificates/0fb5d928-700d-4704-81fe-304e48121475",
+      linkText: "See certificate"
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/morded",
+    email: "gab.jenei@gmail.com",
+    linkedin: "https://www.linkedin.com/in/gabor-jenei-599b9a253/",
+  },
+
+  projects: [
+    {
+      title: "Morded Kanban",
+      isFeatured: true,
+      thumbnail: "/assets/images/mordedkanban.png",
+      githubUrl: "https://github.com/Morded/kanban",
+      liveUrl: "https://morded-kanban.vercel.app/",
+    },
+    {
+      title: "What ToDO",
+      isFeatured: true,
+      thumbnail: "/assets/images/tododark.png",
+      githubUrl: "https://github.com/Morded/whattodo",
+      liveUrl: "https://whattodoornottodo.vercel.app/",
+    },
+    {
+      title: "Guess the hex",
+      isFeatured: true,
+      thumbnail: "/assets/images/guessthehex.png",
+      githubUrl: "https://github.com/Morded/guessthehex",
+      liveUrl: "https://guess-thehex.vercel.app/",
+    },
+    {
+      title: "Search for words",
+      isFeatured: true,
+      thumbnail: "/assets/images/searchforaword.png",
+      githubUrl: "https://github.com/Morded/search",
+      liveUrl: "https://searchforwords.vercel.app/",
+    },
+  ],
+  skills: [
+    {
+      name: "ReactJS",
+      icon: "/assets/images/react.png"
+    },
+    {
+      name: "NextJS",
+      icon: "/assets/images/next.png"
+    },
+    {
+      name: "Javascript",
+      icon: "/assets/images/js.png"
+    },
+    {
+      name: "CSS",
+      icon: "/assets/images/css.png"
+    },
+    {
+      name: "HTML",
+      icon: "/assets/images/html.png"
+    },
+    {
+      name: "SQL",
+      icon: "/assets/images/sql.png"
+    },
+    {
+      name: "PHP",
+      icon: "/assets/images/php.png"
+    },
+    {
+      name: "Delphi Object Pascal",
+      icon: "/assets/images/delphi.png"
+    },
+    {
+      name: "Python",
+      icon: "/assets/images/python.png"
+    },
+    {
+      name: "C language",
+      icon: "/assets/images/c.png"
+    },
+    {
+      name: "NodeJS",
+      icon: "/assets/images/nodejs.png"
+    },
+    {
+      name: "Git",
+      icon: "/assets/images/git.png"
+    },
+    {
+      name: "NeoVim",
+      icon: "/assets/images/neovim.png"
+    },
+    {
+      name: "Figma",
+      icon: "/assets/images/figma.png"
+    },
+  ],
+};
